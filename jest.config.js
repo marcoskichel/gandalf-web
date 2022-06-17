8cb9c673c9e5e755fe8a683144928fa96da11ccc
Marcos Kichel <marcos.kichel@gmail.com>
2022-06-17 00:06:08 -0300

    Removed babel

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  testEnvironment: "jsdom"
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
 };
+
+module.exports = createJestConfig(customJestConfig);
