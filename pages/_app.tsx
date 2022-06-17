9faa94a84b33196aea1cedb3b762f1a307e20f3e
Marcos Kichel <marcos.kichel@gmail.com>
2022-06-16 23:56:51 -0300

    Fixed imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import type { AppProps } from "next/app";
 import { AuthContextProvider } from "../contexts/AuthContext";
-import "../styles/globals.css";
+import { ThemesContextProvider } from "../contexts/ThemesContext";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { ThemesContextProvider } from "../contexts/ThemesContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
